b3bb66ee8f8dc5a9e74638abdf5595a55afe845b
Kirill Fomichev <fanatid@ya.ru>
2018-06-27 14:12:19 +0300

    Fetch txid of contract creation from blockchair.com

--- bin/etherscan.js
+++ bin/etherscan.js
@@ -8,7 +8,6 @@ const blockchain = require('../lib/blockchain')
 const contractLoader = require('../lib/contract-loader')
 const compilersMap = require('../lib/compilers-map')
 const etherscanConstructorArguments = require('../lib/etherscan-constructor-arguments')
-const etherscanCreationTxIds = require('../lib/etherscan-creation-txids')
 
 function getArgs () {
   return yargs
@@ -51,22 +50,35 @@ function getArgs () {
 }
 
 const fetchText = (() => {
-  let last = Date.now()
-  return makeConcurrent(async (url) => {
-    const sleep = 1000 - (Date.now() - last)
-    await new Promise((resolve) => setTimeout(resolve, sleep))
-
+  async function makeReq (url) {
     const res = await fetch(url)
     assert.equal(res.status, 200)
-    const text = await res.text()
-    last = Date.now()
-    return text
-  }, { concurrency: 1 })
+    return res.text()
+  }
+
+  const map = {}
+  return (url, id) => {
+    if (!id) return makeReq(url)
+
+    if (!map[id]) {
+      let last = 0
+      map[id] = makeConcurrent(async (url) => {
+        const sleep = 1000 - (Date.now() - last)
+        await new Promise((resolve) => setTimeout(resolve, sleep))
+
+        const res = await makeReq(url)
+        last = Date.now()
+        return res
+      }, { concurrency: 1 })
+    }
+    return map[id](url)
+  }
 })()
 
-const soljsonVersions = {}
 async function soljsonVersionsLoad () {
   const text = await fetchText('https://raw.githubusercontent.com/ethereum/solc-bin/gh-pages/bin/list.txt')
+
+  const soljsonVersions = {}
   for (const line of text.split('\n')) {
     const parsed = line.match(/([a-z0-9]+)\.js$/)
     if (!parsed) continue
@@ -76,6 +88,7 @@ async function soljsonVersionsLoad () {
 
     soljsonVersions[commit] = line.trim().slice(9, -3)
   }
+  return soljsonVersions
 }
 
 function onExistsUpdate () {
@@ -83,16 +96,8 @@ function onExistsUpdate () {
   process.exit(0)
 }
 
-async function fetchAddress (address, { update }) {
-  address = address.toLowerCase()
-  const exists = await contractLoader.existsByAddressNetwork(address, 'foundation')
-  if (exists) {
-    if (update) onExistsUpdate()
-    return console.log(logSymbols.info, `Already exists: ${address}`)
-  }
-
-  const html = await fetchText(`https://etherscan.io/address/${address}`)
-  console.log(logSymbols.info, `Load address ${address}`)
+async function fetchAddressEtherscan (address) {
+  const html = await fetchText(`https://etherscan.io/address/${address}`, 'etherscan')
 
   const $ = cheerio.load(html)
   const table = $('div#ContentPlaceHolder1_contractCodeDiv table')
@@ -107,37 +112,63 @@ async function fetchAddress (address, { update }) {
   const src = code.find('pre#editor').text().trim()
   const abi = code.find('pre#js-copytextarea2').text().trim()
 
-  let cargs = etherscanConstructorArguments[address] || ''
+  let cargs = ''
   const cargsHTML = $(code.find('pre')[3]).html()
   if (cargsHTML) {
     const match = cargsHTML.match(/^([0-9a-f]+)</)
     assert.ok(match && match[1].length % 2 === 0, 'Contructor Arguments is not valid')
     cargs = match[1]
   }
+  cargs = cargs || etherscanConstructorArguments[address] || ''
 
-  const txid = $('#ContentPlaceHolder1_trContract > td:nth-child(2) > span > a').text().trim() || etherscanCreationTxIds[address]
+  return { name, compiler, optimise, src, abi, cargs }
+}
+
+async function fetchAddressBlockchair (address) {
+  const text = await fetchText(`https://api.blockchair.com/ethereum/calls?q=recipient(${address}),type(create)`, 'blockchair')
+  const result = JSON.parse(text)
+  assert.equal(result.data.length, 1, `Expected not more than 1 rows, received: ${result.data.length}`)
+
+  const txid = result.data[0].transaction_hash
   const txInfo = await blockchain.getTxInfo('foundation', txid)
   assert.equal(txInfo.address, address)
 
-  const compilerMark = compiler.match(/([a-z0-9]+)$/)[1].slice(0, 6)
+  return { txid, bin: txInfo.bin }
+}
+
+async function fetchAddress (address, { soljsonVersions, update }) {
+  address = address.toLowerCase()
+  const exists = await contractLoader.existsByAddressNetwork(address, 'foundation')
+  if (exists) {
+    if (update) onExistsUpdate()
+    return console.log(logSymbols.info, `Already exists: ${address}`)
+  }
+
+  const [etherscan, blockchair] = await Promise.all([
+    fetchAddressEtherscan(address),
+    fetchAddressBlockchair(address)
+  ])
+  console.log(logSymbols.info, `Load address ${address}`)
+
+  const compilerMark = etherscan.compiler.match(/([a-z0-9]+)$/)[1].slice(0, 6)
   const soljsonVersion = soljsonVersions[compilerMark] || soljsonVersions[compilersMap[compilerMark]]
   assert.ok(soljsonVersion, `Compiler version should be defined for ${address}`)
 
   const contract = {
     src: {
-      [`${name}.sol`]: src
+      [`${etherscan.name}.sol`]: etherscan.src
     },
-    abi,
-    bin: txInfo.bin,
+    abi: etherscan.abi,
+    bin: blockchair.bin,
     info: {
-      name,
-      entrypoint: `${name}.sol`,
+      name: etherscan.name,
+      entrypoint: `${etherscan.name}.sol`,
       compiler: soljsonVersion,
-      optimise,
+      optimise: etherscan.optimise,
       network: 'foundation',
-      txid,
+      txid: blockchair.txid,
       address,
-      constructor: cargs
+      constructor: etherscan.cargs
     }
   }
   const added = await contractLoader.save(contract)
@@ -147,7 +178,7 @@ async function fetchAddress (address, { update }) {
 }
 
 async function fetchPage (page, options) {
-  const html = await fetchText(`https://etherscan.io/contractsVerified/${page}`)
+  const html = await fetchText(`https://etherscan.io/contractsVerified/${page}`, 'etherscan')
   console.log(logSymbols.info, `Load page ${page}`)
 
   const $ = cheerio.load(html)
@@ -168,9 +199,9 @@ async function fetchPages (down, up, options) {
 
 async function main () {
   const args = getArgs()
-  await soljsonVersionsLoad()
+  const soljsonVersions = await soljsonVersionsLoad()
 
-  const options = { update: args.update }
+  const options = { soljsonVersions, update: args.update }
   if (args.address) await fetchAddress(args.address, options)
   if (args.page) await fetchPage(args.page, options)
   if (args.pages) await fetchPages(...args.pages, options)
